6c22ef180927c464beafb4edb105731b4d91ccc1
catcake-deploy <deploy@local>
2026-08-02 22:55:00 +0800

    update: builtin font + audio preload

--- web/flutter_bootstrap.js
+++ web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"0cd610717bde95fd88343c64f81c11ba4e5c00
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2561384988" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1080709469" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
